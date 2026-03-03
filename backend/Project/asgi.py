89ff0ba455757af7e6698aef760358c79396284e
Arsh910 <arshdeeppalial@gmail.com>
2026-03-03 10:31:46 +0530

    Fix ASGI initialization order for Uvicorn

--- backend/Project/asgi.py
+++ backend/Project/asgi.py
@@ -8,16 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
 import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from server import routing as server_routing
 from ecomeets import routing as ecomeets_routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
