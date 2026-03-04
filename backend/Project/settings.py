c01497ed798cd408d54cd7a408bb469ab13af062
Arsh910 <arshdeeppalial@egmail.com>
2026-03-04 13:33:56 +0530

    settings

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -118,23 +118,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env("DB_NAME", default="devdb"),
-#         'USER': env("DB_USER", default="devuser"),
-#         'PASSWORD': env("DB_PASS", default="changeme"),
-#         'HOST': env("DB_HOST", default="localhost"),
-#         'PORT': env("DB_PORT", default="5432"),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DB_NAME", default="devdb"),
+        'USER': env("DB_USER", default="devuser"),
+        'PASSWORD': env("DB_PASS", default="changeme"),
+        'HOST': env("DB_HOST", default="localhost"),
+        'PORT': env("DB_PORT", default="5432"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
