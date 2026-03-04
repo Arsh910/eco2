ce1d575b1738ceed3198a419b7b174588f2e2ece
Arsh910 <arshdeeppalial@egmail.com>
2026-03-04 13:53:58 +0530

    something

--- backend/Project/asgi.py
+++ backend/Project/asgi.py
@@ -16,16 +16,14 @@ from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
 from server import routing as server_routing
 from ecomeets import routing as ecomeets_routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        URLRouter(
-            server_routing.websocket_urlpatterns + 
-            ecomeets_routing.websocket_urlpatterns
-        )
+    "websocket": URLRouter(
+        server_routing.websocket_urlpatterns + 
+        ecomeets_routing.websocket_urlpatterns
     ),
 })
