7fa34203741eb312aa80f5e0b80dac0d6e04108c
Arsh910 <arshdeeppalial@gmail.com>
2026-03-05 14:49:29 +0530

    oauth fix

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -220,7 +220,9 @@ CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND", default="redis://localhost:6379/0")
 
 STORAGES = {
-    # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
