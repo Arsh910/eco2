a572ef4882b1af4dabab30d1c7aa9e2f656f5c2c
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2026-03-19 03:02:28 +0530

    Register Feedback model in admin panel

--- backend/settings/admin.py
+++ backend/settings/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import BackgroundSetting
+from .models import Feedback
 
 # Register your models here.
 admin.site.register(BackgroundSetting)
+admin.site.register(Feedback)
